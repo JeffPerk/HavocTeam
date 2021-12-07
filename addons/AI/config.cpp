9f962469053df476094840a6bd5a753e6600ad97
YonV <32718226+YonVclaw@users.noreply.github.com>
2021-12-06 19:35:55 -0700

    remvoe tabs

--- addons/AI/config.cpp
+++ addons/AI/config.cpp
@@ -1,32 +1,32 @@
 #include "script_component.hpp"
 
 class CfgPatches {
-    class HAVOC_AI {
-        author = AUTHOR;
-        name = NAME;
-        url = URL;
-        units[] = {};
-        requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {
-            "cba_main",
-            "cba_xeh"
-        };
-        version = VERSION;
-        authors[] = {"MitchJC"};
-        weapons[] = {};
-    };
+	class HAVOC_AI {
+		author = AUTHOR;
+		name = NAME;
+		url = URL;
+		units[] = {};
+		requiredVersion = REQUIRED_VERSION;
+		requiredAddons[] = {
+			"cba_main",
+			"cba_xeh"
+		};
+		version = VERSION;
+		authors[] = {"MitchJC"};
+		weapons[] = {};
+	};
 };
 
 #include "CfgVehicles.hpp"
 
 class CfgFunctions {
-    #include "CfgFunctions.hpp"
+	#include "CfgFunctions.hpp"
 };
 
 class Extended_PreInit_EventHandlers {
-    class HAVOC_AI_Event {
-        init = "call compile preProcessFileLineNumbers 'z\havoc\addons\AI\XEH_preInit.sqf'";
-    };
+	class HAVOC_AI_Event {
+		init = "call compile preProcessFileLineNumbers 'z\havoc\addons\AI\XEH_preInit.sqf'";
+	};
 };
 
 
